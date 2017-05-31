9eca1eb55c35458b738eb5adfd6fc83b1e94e7b4
dwaypro <dangrantway@gmail.com>
2017-05-31 12:47:17 -0700

    added clarification to the questions

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -21,10 +21,10 @@ until employees == number
 	puts "What year were you born?"
 	year = gets.chomp
 	
-	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+	puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 	response = gets.chomp
 
-	puts "Would you like to enroll in the company's health insurance?"
+	puts "Would you like to enroll in the company's health insurance? (y/n)"
 	health = gets.chomp
 
 	if age.to_i != (2017 - year.to_i) && (response == "n" && health == "n")
