545a890ddb63507697195cbfa9de018535b360ea
dwaypro <dangrantway@gmail.com>
2017-06-21 10:52:46 -0700

    Release one done

---
+++ js/algos.js
@@ -0,0 +1,41 @@
+function LongestPhrase(array){
+var length = 0;
+var longest;
+
+// loop
+	for (var i = 0; i < array.length; i ++){
+// conditional
+		if (array[i].length > length){
+		var length = array[i].length;
+		longest = array[i];
+	}
+
+}
+console.log("the longest phrase in the array:" +longest)
+}
+
+
+
+longestphrase = new LongestPhrase(["once upon a time", "twice upon a couple times", "coffee is my fav"])
+longestphrase = new LongestPhrase(["wide eyed with a heart filled with freight", "You were all I ever longed for", "haha Mumford lyrics"])
+
+
+function KeyChecker(obj, obj2){
+var length = 0;
+
+
+
+
+//loop
+//for (var i = 0; i < obj.length; i ++){
+
+
+//conditional
+
+//}
+
+}
+
+keychecker = new KeyChecker({name: "Steven", age: 54}, {name: "Tamir", age: 54});
+
+
